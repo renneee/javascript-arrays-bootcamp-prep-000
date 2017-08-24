eadfeb456510b49bcdb5f10e4426d07eb800f753
renneee <renpyang@gmail.com>
2017-08-24 06:56:27 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,1 +1,26 @@
 var chocolateBars = ["snickers","hundred grand", "kitkat", "skittles" ]
+
+function addElementToBeginningOfArray (fruit) {
+  var fruit = ["apple", "orange"]
+  fruit = ["pear", ...fruit]
+  return fruit
+}
+
+function destructivelyAddElementToBeginningOfArray (fruit, addFruit) {
+  const fruit = ["apple", "orange"]
+  const addFruit = ["pear", ...fruit]
+  return addFruit
+}
+
+function addElementToEndOfArray (fruit) {
+  var fruit = ["apple", "orange"]
+  var fruit = [...fruit, "pear"]
+  return fruit
+}
+
+function destructivelyAddElementToEndOfArray (fruit, anotherFruit) {
+  const fruit = ["apple", "orange"]
+  const anotherFruit = [...fruit, "pear"]
+  return anotherFruit
+}
+
