f6678ae3bb81f9b321a3766b96afeb7eefce50ee
renneee <renpyang@gmail.com>
2017-08-24 09:36:10 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -31,6 +31,11 @@ function removeElementFromBeginningOfArray (array) {
 }
 
 function destructivelyRemoveElementFromEndOfArray(array) {
-  array.push()
+  array.pop()
   return array
 }
+
+function removeElementFromEndOfArray (array) {
+  return array.slice(0, array.length - 1)
+}
+
