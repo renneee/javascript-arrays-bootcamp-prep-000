041bcbdff0c788748d6464bd0b63e44f2754481f
renneee <renpyang@gmail.com>
2017-08-24 09:30:35 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -15,3 +15,18 @@ function addElementToEndOfArray (array, element) {
 function destructivelyAddElementToEndOfArray (array, element) {
   return array.push(element)
 }
+
+function accessElementInArray (array, index) {
+  return array[index]
+}
+
+function destructivelyRemoveElementFromBeginningOfArray (array) {
+  array.shift()
+  return array
+}
+
+function removeElementFromBeginningOfArray (array) {
+  array.slice(1)
+  return array
+}
+
