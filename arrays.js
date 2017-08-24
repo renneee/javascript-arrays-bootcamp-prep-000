cae74b2bc6f48a5cf5b6cec1a69af2d926de677a
renneee <renpyang@gmail.com>
2017-08-24 09:25:12 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -7,3 +7,12 @@ function addElementToBeginningOfArray (array, element) {
 function destructivelyAddElementToBeginningOfArray (array, element) {
   return array.unshift(element)
 }
+
+function addElementToEndOfArray (array, element) {
+  return [...array, element]
+}
+
+function destructivelyAddElementToEndOfArray (array, element) {
+  return array.push(element)
+}
+
