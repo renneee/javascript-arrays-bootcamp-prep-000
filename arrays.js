83425e5d6eef07b09f3367ff57f1f1b7503fbc04
renneee <renpyang@gmail.com>
2017-08-24 09:21:02 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -2,57 +2,43 @@ var chocolateBars = ["snickers","hundred grand", "kitkat", "skittles" ]
 
 function addElementToBeginningOfArray(array, element) {
   return [element, ...array]
+}
 
-function addElementToBeginningOfArray (array) {
-  var array = ["apple", "orange"]
-  array = ["pear", ...array]
+function destructivelyAddElementToBeginningOfArray(array, element){
+  array.unshift(element)
   return array
 }
 
-function destructivelyAddElementToBeginningOfArray (array, element) {
-  const array = ["apple", "orange"]
-  const element = ["pear", ...array]
-  return element
+function addElementToEndOfArray(array, element) {
+  return [...array, element]
 }
 
-function addElementToEndOfArray (fruit) {
-  var fruit = ["apple", "orange"]
-  var fruit = [...fruit, "pear"]
-  return fruit
+function destructivelyAddElementToEndOfArray(array, element) {
+  array.push(element)
+
+  return array
 }
 
-function destructivelyAddElementToEndOfArray (fruit, anotherFruit) {
-  const fruit = ["apple", "orange"]
-  const anotherFruit = [...fruit, "pear"]
-  return anotherFruit
+function accessElementInArray(array, index) {
+  return array[index]
 }
 
-function accessElementInArray (){
-  var fastfood = ["McDonald's", "KFC", "Chipotle"]
-  var bio = " is the best!"
-  return fastfood [2] + bio
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  array.shift()
+
+  return array
 }
 
-function destructivelyRemoveElementFromBeginningOfArray (){
-  var fruits = ["apple", "orange", "pear"]
-  fruits.shift()
-  return fruits
+function removeElementFromBeginningOfArray(array) {
+  return array.slice(1)
 }
 
-function removeElementFromBeginningOfArray () {
-  var fruits = ["apple", "orange", "pear"]
-  fruits.slice(1)
-  return fruits
+function destructivelyRemoveElementFromEndOfArray(array) {
+  array.pop()
+
+  return array
 }
 
-function destructivelyRemoveElementFromEndOfArray () {
-  var fruits = ["apple", "orange", "pear"]
-  fruits.pop()
-  return fruits
-}
-
-function removeElementFromEndOfArray () {
-  var fruits = ["apple", "orange", "pear"]
-  fruits.slice(0, fruits.Length - 1)
-  return fruits
+function removeElementFromEndOfArray(array) {
+  return array.slice(0, array.length - 1)
 }
